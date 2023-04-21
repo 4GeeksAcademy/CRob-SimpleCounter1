1325d924ad92770b167e9aa711c21e10e453e366
Cat <126123826+CRob001@users.noreply.github.com>
2023-04-21 14:43:34 +0000

    Simple Counter Final Project

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,49 @@
 //import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
-import 'bootstrap';
+import './component/icons.js';
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-function SimpleCounter(props){
+import 'bootstrap/dist/css/bootstrap.css';
+
+    function SimpleCounter(props){
+
     return (<div className="bigCounter">
-                <div className="calendar">
-                <i className="fa-regular fa-clock"></i>
-                </div>
-                <div className="four"></div>
-                <div className="three"></div>
-                <div className="two"></div>
-                <div className="one"></div>
-        </div>);
+            <div className="calendar">
+                <i className="far fa-clock"></i>
+            </div>
+            <div className="four">{props.numFour %10}</div>
+            <div className="three">{props.numThree %10}</div>
+            <div className="two">{props.numTwo %10}</div>
+            <div className="one">{props.numOne % 10}</div>
+    </div>);
 }
 
+SimpleCounter.propTypes = {
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number
+}
+
+let counter = 0; 
+
+setInterval(function(){
+
+const four = Math.floor(counter/1000);
+const three = Math.floor(counter/100);
+const two = Math.floor(counter/10);
+const one = Math.floor(counter/1);
+console.log(four, three, two, one);
+
+counter++;
+
 //render your react application
-ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+    ReactDOM.render(<SimpleCounter numOne={one} numTwo={two} numThree={three} numFour={four} />,
+    document.querySelector("#app")
+    );
+}, 1000);
